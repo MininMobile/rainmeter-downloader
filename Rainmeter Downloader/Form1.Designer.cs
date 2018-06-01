1e00b886c0488eb422a9506da67da0b372580d07
MininMobile <miningamer@gmail.com>
2018-06-01 23:36:30 +0100

    Rename Namespace

--- Rainmeter Downloader/Form1.Designer.cs
+++ Rainmeter Downloader/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Rainmeter_Downloader {
+﻿namespace RMD {
     partial class Form1 {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(373, 450);
+            this.ClientSize = new System.Drawing.Size(355, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
